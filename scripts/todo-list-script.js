bc90206433daa627136865829bba1fa9cea204f2
Loryn Hartbarger <lehartbarger@gmail.com>
2023-12-17 08:13:15 -0500

    changes

--- scripts/todo-list-script.js
+++ scripts/todo-list-script.js
@@ -1,49 +1,45 @@
-  const toDoList = [{
-    task:'',
-    dueDate:'',
-    time:''
-    }]
+const toDoList = [];
 
-  function renderToDoList(){
-    let toDoListHTML = '';
+function renderToDoList() {
+  const toDoListContainer = document.querySelector('.js-to-do-list');
+  toDoListContainer.innerHTML = ''; // Clear existing content
 
-    toDoList.forEach((toDoObject, index) => {
-      const {task, dueDate, time} = toDoObject;
-      const HTML = `
-        <div>${task}</div>  
-        <div>${dueDate}</div>  
-        <div>${time}</div>  
-        <button class="delete-button js-delete-button">Delete</button>
-      `;
-      toDoListHTML += HTML;
+  toDoList.forEach((toDoObject, index) => {
+    const { task, dueDate, time } = toDoObject;
+    const taskElement = document.createElement('div');
+    taskElement.innerHTML = `${task} ${dueDate} ${time}`;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-button', 'js-delete-button');
+    deleteButton.textContent = 'Delete';
+
+    deleteButton.addEventListener('click', () => {
+      toDoList.splice(index, 1);
+      renderToDoList();
     });
-  
-    document.querySelector('.js-to-do-list').innerHTML = toDoListHTML;
-    document.querySelectorAll('.js-delete-button').forEach((deleteButton, index) => {
-      deleteButton.addEventListener('click', () => {
-        toDoList.splice(index,1);
-        renderToDoList();
-      });
-    });
-  }
 
-  document.querySelector('.js-add-button').addEventListener('click', () => {
-    addTask();
+    taskElement.appendChild(deleteButton);
+    toDoListContainer.appendChild(taskElement);
   });
+}
 
-  function addTask(){
-    const inputElement = document.querySelector('.js-taskname-input');
-    const task = inputElement.value;
-    const dueDateInput = document.querySelector('.js-duedate-input');
-    const dueDate = dueDateInput.value;
-    const timeInput = document.querySelector('.js-time-input');
-    const time = timeInput.value;
+document.querySelector('.js-add-button').addEventListener('click', () => {
+  addTask();
+});
 
-    toDoList.push({
-      task,
-      dueDate,
-      time
-    });
-    inputElement.value='';
-    renderToDoList();
-  }
+function addTask() {
+  const inputElement = document.querySelector('.js-taskname-input');
+  const task = inputElement.value;
+  const dueDateInput = document.querySelector('.js-duedate-input');
+  const dueDate = dueDateInput.value;
+  const timeInput = document.querySelector('.js-time-input');
+  const time = timeInput.value;
+
+  toDoList.push({
+    task,
+    dueDate,
+    time
+  });
+  inputElement.value = '';
+  renderToDoList();
+}
